9c739332a04403a2ea9def6fcbfcb1db2397fe64
Chitraksh Grover <chitrakshgrover@gmail.com>
2020-03-26 00:40:10 +0530

    navigate and logo component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React , { Component } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+class App extends Component{
+	render(){
+		return(
+			<div className="App">
+				<Navigation/>
+				<Logo/>
+			{/*	<ImageLinkForm/>
+				<FaceRecognition/>*/}	
+			</div>
+		);
+	}
+}
+
+export default App;
+

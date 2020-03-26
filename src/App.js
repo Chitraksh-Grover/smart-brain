feb29f3c8b45b1f4db4ab0a05814b96e87e2e285
Chitraksh Grover <chitrakshgrover@gmail.com>
2020-03-26 23:40:17 +0530

    basic components completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React , { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+
 class App extends Component{
 	render(){
 		return(
 			<div className="App">
 				<Navigation/>
 				<Logo/>
-			{/*	<ImageLinkForm/>
-				<FaceRecognition/>*/}	
+				<Rank/>
+				<ImageLinkForm/>
+		{/*		<FaceRecognition/>*/}	
 			</div>
 		);
 	}
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import brain from './brain.png';
 
 const Logo = () =>{
 	return(
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 150, width: 150 }} >
-				 <div className="Tilt-inner"> 👽 </div>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 100, width: 100 }} >
+				 <div className="Tilt-inner pa3"><img style={{paddingTop:'4px'}} alt='logo' src = {brain}/></div>
 			</Tilt>
 		</div>
 	);

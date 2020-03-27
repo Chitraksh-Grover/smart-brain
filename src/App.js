db079180ced30cd5275d91414ba857c7fd7891ce
Chitraksh Grover <chitrakshgrover@gmail.com>
2020-03-27 22:05:34 +0530

    particles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const options = {
+	    "particles": {
+	        "number": {
+	            "value": 55
+	        },
+	        "size": {
+	            "value": 3 
+	        }
+	    }
+}
 
 class App extends Component{
 	render(){
 		return(
 			<div className="App">
+				<Particles className = 'particle' params = {options}/>
 				<Navigation/>
 				<Logo/>
 				<Rank/>
